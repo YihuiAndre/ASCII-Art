080465dc3258f765ac31f874fe60fbec73173b10
YihuiAndre <yihuiawuchen@gmail.com>
2020-09-30 22:09:19 -0400

    update the class to support the image in url and convert characters into png image file

--- Main.java
+++ Main.java
@@ -3,7 +3,8 @@
 public class Main {
     public static void main(String[] args) {
         //RBGCharConvertor container = new RBGCharConvertor(16, "SpecialCharacters.txt");
-        TextImage i = new TextImage(new RBGCharConvertor(256), "img/example_1.jpg", 2);
-        i.toTerminal();
+        TextImage i = new TextImage(new RBGCharConvertor(2, "characters/Alphabet.txt"), "https://thebiem.com/wp-content/uploads/2018/12/New-Movie-Weather-With-You-feature-.jpg", true, 1);
+        i.imgToTextFile("img.txt");;
+        i.imgToImgFile("output.png");
     }
 }
--- TextImage.java
+++ TextImage.java
@@ -1,12 +1,13 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter; // Import the FileWriter class
 import java.io.IOException;
+import java.net.URL;
 import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
-
-import helper.Helper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,45 +19,59 @@ public class TextImage {
 
     //constructor which take three parameter: 
     //1. the translator which translate the color into character
-    //2. the filepath which is the path for image
-    //3. the size of compress which require to compress the color in the image by nxn square
+    //2. the path of image which can be the path for image or URL link
+    //3. the boolean value that indicate whatever the second parameter is a link
+    //4. the size of compress which require to compress the color in the image by nxn square
     //After the execution, store all the RBG value into 2-dimensional arraylist
-    public TextImage(RBGCharConvertor translator, String filepath, int sizeOfCompress){
+
+    //Q: how to import url link or file path
+    public TextImage(RBGCharConvertor translator, String imgPath, boolean isURL, int sizeOfCompress){
         this.translator = translator;
         this.imgRBGArr = new ArrayList<>();
         BufferedImage image = null; 
-        try(FileInputStream stream = new FileInputStream(filepath)){
-            image = ImageIO.read(stream);
-            int w = image.getWidth(); 
-            int h = image.getHeight();
-            System.out.println("width: " + w + " height: " + h);
-            //store the value into arraylist
-            int avgColor = 0, index = 0;
-            //x and y coordinate is stand for the left and right corner coordinate of the square
-            for(int y = 0; y < h; y+=sizeOfCompress){
-                this.imgRBGArr.add(new ArrayList<>());
-                for(int x = 0; x < w; x+=sizeOfCompress){
-                    //prevent the case when it could not form a square by current x and y coordinate
-                    if(y + sizeOfCompress-1 >= h || x + sizeOfCompress-1 >= w){
-                        break;
-                    }
-                    avgColor = compressRBG(x, y, sizeOfCompress, image);
-                    this.imgRBGArr.get(index).add(avgColor);
-                }
-                index++;
+        try{
+            if(isURL){
+                URL url = new URL(imgPath);
+                image = ImageIO.read(url);
             }
-            System.out.println("Reading Complete");
+            else{
+                FileInputStream stream = new FileInputStream(imgPath);
+                image = ImageIO.read(stream);
+            }
         }
         catch(FileNotFoundException err){
-
+            err.printStackTrace();
         }
         catch(IOException err){
-
+            err.printStackTrace();
         }
+        catch(NullPointerException err){
+            err.printStackTrace();
+        }
+        int w = image.getWidth(); 
+        int h = image.getHeight();
+        System.out.println("width: " + w + " height: " + h);
+        //store the value into arraylist
+        int avgColor = 0, index = 0;
+        //x and y coordinate is stand for the left and right corner coordinate of the square
+        for(int y = 0; y < h; y+=sizeOfCompress){
+            this.imgRBGArr.add(new ArrayList<>());
+            for(int x = 0; x < w; x+=sizeOfCompress){
+                //prevent the case when it could not form a square by current x and y coordinate
+                if(y + sizeOfCompress-1 >= h || x + sizeOfCompress-1 >= w){
+                    break;
+                }
+                avgColor = compressRBG(x, y, sizeOfCompress, image);
+                this.imgRBGArr.get(index).add(avgColor);
+            }
+            index++;
+        }
+        System.out.println("Reading Complete");
     }
 
-    public TextImage(RBGCharConvertor translator, String filepath){
-        this(translator, filepath, 1);
+    //overload constructor which miss the size of compression
+    public TextImage(RBGCharConvertor translator, String imgPath, boolean isURL){
+        this(translator, imgPath, isURL, 1);
     }
 
     //compress mutiple RBG values inside the square into one value
@@ -87,10 +102,10 @@ public class TextImage {
             }
         }
         catch(FileNotFoundException err){
-
+            err.printStackTrace();
         }
         catch(IOException err){
-
+            err.printStackTrace();
         }
     }
 
@@ -105,20 +120,45 @@ public class TextImage {
             }
         }
         catch(FileNotFoundException err){
-
+            err.printStackTrace();
         }
         catch(IOException err){
-
+            err.printStackTrace();
         }
     }
 
     //output the img to the terminal
-    public void toTerminal(){
+    public void print(){
         for(List<Integer> array : this.imgRBGArr){
             for(Integer val : array){
                 System.out.print(translator.RBGToChar(val));
             }
             System.out.println("");
+        }
+    }
+
+    //take the text image and convert it into png image format
+    public void imgToImgFile(String filePath){
+        int width = 15*this.imgRBGArr.size(), height = 15*this.imgRBGArr.size();
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics g = image.getGraphics();
+        //change the pen color
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, width, height);
+        g.setColor(Color.BLACK);
+        g.setFont(g.getFont().deriveFont(18f));
+        for(int row = 0, rLen = this.imgRBGArr.size(); row < rLen; row++){
+            for(int col = 0, cLen = this.imgRBGArr.get(row).size(); col < cLen; col++){
+                char character = translator.RBGToChar(this.imgRBGArr.get(row).get(col));
+                g.drawString(String.valueOf(character), 10*col, 15+row*15);
+            }
+        }
+        g.dispose();
+        try {
+            //write file, but only work for png
+            ImageIO.write(image, "png", new File(filePath));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
