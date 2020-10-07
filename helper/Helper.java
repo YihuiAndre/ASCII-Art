45a7c9c752d2958d5e027a4c069be7eae577c6a8
YihuiAndre <yihuiawuchen@gmail.com>
2020-10-07 16:34:44 -0400

    add an helper function to sum the array value

--- helper/Helper.java
+++ helper/Helper.java
@@ -1,5 +1,6 @@
 package helper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Helper {
@@ -10,5 +11,14 @@ public class Helper {
             }
             System.out.println("");
         }
+    }
+
+    //helper function
+    public static int sum(int[] arr){
+        int sum = 0;
+        for(int i = 0, len = arr.length; i < len; i++){
+            sum += arr[i];
+        }
+        return sum;
     }
 }
