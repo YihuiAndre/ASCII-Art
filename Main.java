fbfd1649126f84f25304a31b0e70b5535894a6f9
YihuiAndre <yihuiawuchen@gmail.com>
2020-10-07 16:36:56 -0400

    update Main class

--- Main.java
+++ Main.java
@@ -6,24 +6,27 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, NullPointerException, IOException, Exception {
         //ColorConvertor container = new ColorConvertor(16, "SpecialCharacters.txt");
-        TextImage a = new TextImage(new ColorConvertor(64, "characters/ASCII.txt"), "https://thebiem.com/wp-content/uploads/2018/12/New-Movie-Weather-With-You-feature-.jpg", true, 1);
+        //TextImage a = new TextImage(new ColorConvertor(256, "characters/ASCII.txt"), "https://thebiem.com/wp-content/uploads/2018/12/New-Movie-Weather-With-You-feature-.jpg", true, 1);
+        //TextImage a = new TextImage(new ColorConvertor(256, "characters/ASCII.txt"), "https://static.wikia.nocookie.net/darling-in-the-franxx/images/2/27/Kv04.jpg/revision/latest?cb=20171209072941", true, 2);
+        //TextImage a = new TextImage(new ColorConvertor(256, "characters/ASCII.txt"), "img/image.jpg", false, 1);
         //a.toTextFile("img.txt");
         //a.RGBAToTextFile("RBG.txt");
-        a.toImgFile("output.png");
+        //a.toImgFile("output.png");
         //a.print();
-
-        TextImage[] listOfImage = new TextImage[10];
-        ColorConvertor translator = new ColorConvertor(32, "characters/ASCII.txt");
+        
+        ColorConvertor translator = new ColorConvertor(256, "characters/ASCII.txt");
         int index = 0;
-        File dir = new File("gif/");
+        File dir = new File("gif/anime/");
         File[] directoryListing = dir.listFiles();
         Arrays.sort(directoryListing);
+        TextImage[] listOfImage = new TextImage[directoryListing.length];
         if (directoryListing != null) {
             for (File child : directoryListing) {
                 // Do something with child
                 System.out.println(child.getPath());
                 listOfImage[index] = new TextImage(translator, child.getPath(), false);
-                listOfImage[index].toImgFile("output/" + index + ".png");
+                int num = Integer.parseInt(child.getName().replaceAll("[\\D]", ""));
+                listOfImage[index].toImgFile("output/anime/" + num + ".png");
                 index++;
             }
         } else {
