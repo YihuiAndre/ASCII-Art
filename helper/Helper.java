c5a12c8c46f052d60f31a3eaba879114ed95a6e5
YihuiAndre <yihuiawuchen@gmail.com>
2020-10-09 21:22:31 -0400

    Create function to display task bar

--- helper/Helper.java
+++ helper/Helper.java
@@ -1,6 +1,5 @@
 package helper;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Helper {
@@ -20,5 +19,21 @@ public class Helper {
             sum += arr[i];
         }
         return sum;
+    }
+
+    //print the task bar to display if the work is done or not
+    public static void printTaskBar(double finished, double total, String description){
+        int percentage = (int) ((finished / total) * 100);
+        StringBuilder bar = new StringBuilder();
+        bar.append("|");
+        for(int i = 0; i < 50; i++){
+            if(i < percentage/2) bar.append("=");
+            else bar.append(" ");
+        }
+        bar.append("|" + String.format("%02d", percentage) + "%\r");
+        System.out.print(bar);
+        if(percentage == 100){
+            System.out.println("\n" + description);
+        }
     }
 }
