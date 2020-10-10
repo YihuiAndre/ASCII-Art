8692d70805d055c96ef03c7d9dfc14ce661c35c5
YihuiAndre <yihuiawuchen@gmail.com>
2020-10-10 01:22:49 -0400

    update file to handle special case when user enter wrong input

--- Main.java
+++ Main.java
@@ -8,9 +8,7 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException, NullPointerException, IOException, Exception {
         String inputPath = "", outputPath  = "", textFile = "characters/ASCII.txt";
         int numOfChar = 256, compressSize = 1;
-        boolean isDirectory =  false;
-        String help = "Operation: -d    Path of input directory.\n"
-                    + "           -i    Path of an image.\n"
+        String help = "Operation: -i    Path of input directory/image.\n"
                     + "           -o    Path of output directory/image.\n"
                     + "           -t    Path of characters file. default value: characters/ASCII.txt\n"
                     + "           -n    Number of unique characters. default value: 256\n"
@@ -20,10 +18,6 @@ public class Main {
         
         for(int i = 0, len = args.length; i < len; i++){
             switch(args[i].charAt(1)){
-                case 'd':
-                    inputPath = args[i+1];
-                    isDirectory = true;
-                    break;
                 case 'i':
                     inputPath = args[i+1];
                     break;
@@ -45,17 +39,42 @@ public class Main {
             }
             i++;
         }
+        File input = new File(inputPath);
+        File output = new File(outputPath);
+        //handle case when input and output file is not valid
+        if(!(input.exists())){
+            System.out.println("Please give a valid input path");
+            return;
+        }
+        else if(!(output.exists())){
+            System.out.println("Please give a valid output path");
+            return;
+        }
         System.out.println(inputPath + "--------------->" + outputPath);
         ColorConvertor translator = new ColorConvertor(numOfChar, textFile);
-        if(!isDirectory){
+        if(!input.isDirectory()){
+            //if output path is not a file path, print error message
+            if(!(output.isFile())){
+                System.out.println("Please give a valid output image path");
+                return;
+            }
+            //validate the file type, only allow png image
+            else if(!outputPath.substring(outputPath.length() - 3).equals("png")){
+                System.out.println("Please make sure the image file extension is ending by png");
+                return;
+            }
             TextImage img = new TextImage(translator, inputPath, compressSize);
             img.toImgFile(outputPath);
             System.out.println("success import image to " + outputPath);
         }
         else{
+            //if output path is not a directory path, print error message
+            if(!(output.isDirectory())){
+                System.out.println("Please give a valid output directory path");
+                return;
+            }
             int index = 0;
-            File dir = new File(inputPath);
-            File[] directoryListing = dir.listFiles();
+            File[] directoryListing = input.listFiles();
             if (directoryListing != null) {
                 TextImage[] listOfImage = new TextImage[directoryListing.length];
                 for (File child : directoryListing) {
--- execute.sh
+++ execute.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-javac Main.java
+input=${1:- NULL}
+output=${2:- NULL}
+text=${3:-characters/ASCII.txt}
+num=${4:-256}
+compress=${5:-1}
 
-java Main -d gif/bear -o output/bear
+javac Main.java
+if [ "$input" = "h" ]
+then
+    java Main -h
+else
+    java Main -i $input -o $output -t $text -n $num -c $compress
+fi
